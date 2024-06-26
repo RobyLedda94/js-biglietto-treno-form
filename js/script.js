b787b7e3a7aa9e0420d7438b2c9f21ee25ef5777
RobyLedda94 <robyledda01@yahoo.it>
2024-06-26 16:43:41 +0200

    [UPD] add btn prompt

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 // MESSAGGIO ALERT DI PROVA
 alert("Javascript calcolo biglietto treno");
+
+
+
+// VADO A RICHIAMARE I BOTTONI E VERIFICO CON IL CONSOLE LOG SE FUNZIONA CORRETTAMENTE
+
+const btn = document.getElementById('generate-price')
+btn.addEventListener('click', function () {
+    let name = document.getElementById('name').value;
+    let km = document.getElementById('km').value;
+    let eta = document.getElementById('eta').value;
+    console.log(name, km, eta);
+})
